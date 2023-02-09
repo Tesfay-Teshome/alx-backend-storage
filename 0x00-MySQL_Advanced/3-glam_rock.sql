b3796d68ea8d22fe670be178696ff277d314409f
Tesfay-Teshome <tesfix2017@gmail.com>
2023-02-09 09:23:43 +0000

    Old School Band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 --  lists all bands with Glam rock
-SELECT band_name, (IFNULL(split, '2023') - formed) AS lifespan
+SELECT band_name, (IFNULL(formed, '2023') - split) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
     ORDER BY lifespan DESC;
